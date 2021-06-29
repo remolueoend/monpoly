13112a4ddced7b79b06174abc0318a8f3fb0a92c
Nicolas Kaletsch <nicolkal@student.ethz.ch>
2021-06-29 12:30:57 +0200

    finished soundness with auxiliary lemmas

--- thys/MFODL_Monitor_Devel/Type_Soundness.thy
+++ thys/MFODL_Monitor_Devel/Type_Soundness.thy
@@ -9,40 +9,32 @@ lemma safe_neg_eq: "safe_formula (Formula.Neg (Formula.Eq x1 x2)) \<Longrightarr
 (Formula.sat \<sigma> V v i  (Formula.Neg (Formula.Eq x1 x2)) \<longleftrightarrow> sat' \<sigma> V v i  (Formula.Neg (Formula.Eq x1 x2))) "
   by (cases x1; cases x2) auto
 
-lemma foldl_eq_type2:
- " \<forall>x\<in>set (x22). ty_of x = t \<Longrightarrow> ty_of x21 = t \<Longrightarrow> foldl undef_min x21 x22 = foldl min x21 x22"
-
- " \<forall>x\<in>set (x22). ty_of x = t \<Longrightarrow> ty_of x21 = t \<Longrightarrow> foldl undef_max x21 x22 = foldl max x21 x22"
- " \<forall>x\<in>set (x22). ty_of x = t \<Longrightarrow> ty_of x21 = t  \<Longrightarrow> t \<in> numeric_ty
-\<Longrightarrow> foldl undef_plus x21 x22 = foldl (+) x21 x22
-"
-  sorry
-lemma foldl_eq_type: assumes " \<forall>x\<in>set (x22). ty_of x = t" "ty_of x21 = t" "t \<in> agg_trm_type agg_op"
-    "undef_op = undef_min \<and> op = min \<and>  agg_op=Formula.Agg_Min \<or> 
-undef_op = undef_max \<and> op = max\<and>  agg_op=Formula.Agg_Max \<or>
- undef_op = undef_plus \<and> op= (+)\<and>  agg_op=Formula.Agg_Sum"
-  shows "foldl undef_op x21 x22 = foldl op x21 x22" 
+lemma foldl_sound:
+  assumes  " \<forall>x\<in>set (x22). ty_of x = t" "ty_of x21 = t" 
+  shows
+" foldl undef_min x21 x22 = foldl min x21 x22"
+ "foldl undef_max x21 x22 = foldl max x21 x22"
+ "t \<in> numeric_ty \<Longrightarrow> foldl undef_plus x21 x22 = foldl (+) x21 x22 \<and> ty_of (foldl (+) x21 x22 ) = t"
+  
 proof -
-  {assume "undef_op = undef_min \<and> op = min \<or> undef_op = undef_max \<and> op = max"
-    from this assms(1-2) have ?thesis
+  
+    from  assms(1-2) have minmax:"foldl undef_min x21 x22 = foldl min x21 x22 \<and> foldl undef_max x21 x22 = foldl max x21 x22"
      proof (induction x22 arbitrary: x21 t)
        case (Cons a x22)
-       then show ?case   apply (simp add: min_def undef_min_def max_def undef_max_def) 
+       then show  ?case   apply (simp add: min_def undef_min_def max_def undef_max_def) 
          using undef_less_eq_sound  by (cases x21; cases a) fastforce+ 
      qed auto
-   }
-   moreover 
-   {
-     assume "undef_op = undef_plus \<and> op = (+) \<and>  agg_op=Formula.Agg_Sum"
- from this assms(1-3) have ?thesis
+     from this show "foldl undef_min x21 x22 = foldl min x21 x22" by auto
+     from minmax show "foldl undef_max x21 x22 = foldl max x21 x22" by auto
+   next
+     assume "t \<in> numeric_ty"
+     from this assms show "foldl undef_plus x21 x22 = foldl (+) x21 x22 \<and> ty_of (foldl (+) x21 x22 ) = t"
      proof (induction x22 arbitrary: x21 t)
        case (Cons a x22)
        then show ?case using undef_plus_sound   apply (cases x21; cases a) apply (auto simp add: numeric_ty_def)
          
-         using ty_of.simps by blast+
+         using ty_of.simps by presburger+
      qed auto
-   }
-   ultimately show ?thesis  using assms(4) by auto
  qed
 
 lemma eval_agg_op_sound: 
@@ -61,9 +53,23 @@ proof -
        apply (auto simp add:  assms(1)) using finite_fst by (auto simp add: finite_multiset_def assms(1) ) 
       then have wty_flatten: "finite_multiset M \<Longrightarrow> \<forall>x \<in> set (flatten_multiset M) . ty_of x = t"  using wty_M 
         by fastforce
+     have avg:"finite_multiset M \<Longrightarrow>  flatten_multiset M = x21 # x22 \<Longrightarrow>agg_op = Formula.Agg_Avg \<Longrightarrow>
+          double_of_event_data_agg (foldl (+) x21 x22) = undef_double_of_event_data_agg (foldl undef_plus x21 x22)" for x21 x22
+     proof -
+       assume as: "finite_multiset M" "flatten_multiset M = x21 # x22"  "agg_op = Formula.Agg_Avg" 
+       have foldl_plus: "foldl undef_plus x21 x22 = foldl (+) x21 x22" 
+         apply (rule  conjE[OF foldl_sound(3)])  using wty_flatten t_wty as   by auto
+       have foldl_ty: "ty_of (foldl (+) x21 x22) \<in> numeric_ty "  apply (rule conjE[OF  foldl_sound(3)]) using as wty_flatten t_wty by auto
+        show ?thesis apply (auto simp: foldl_plus) apply (cases "foldl (+) x21 x22") using  undef_double_of_event_data_agg_sound foldl_ty by (auto simp add: numeric_ty_def)
+     qed
 
-    show ?thesis 
-      apply (cases agg_op) using wty_flatten t_wty apply (auto split: list.splits) sorry
+      have med:"finite_multiset M \<Longrightarrow> flatten_multiset M = xs \<Longrightarrow> agg_op = Formula.Agg_Med \<Longrightarrow> i< length xs \<Longrightarrow>
+double_of_event_data_agg (xs!i) = undef_double_of_event_data_agg (xs!i) " for xs i 
+        using t_wty wty_flatten undef_double_of_event_data_agg_sound nth_mem[where ?n=i and ?xs=xs] by (cases "xs!i")  (auto simp add: numeric_ty_def split:ty.splits) 
+      then show ?thesis
+      apply (cases agg_op) using wty_flatten t_wty avg apply (auto split: list.splits bool.splits) 
+      using foldl_sound  apply presburger+ 
+      by (smt (z3) Suc_less_eq diff_Suc_1 div2_less_self even_Suc length_nth_simps(2) less_Suc_eq odd_Suc_div_two)
   qed 
 
 lemma soundness:
@@ -234,7 +240,9 @@ next
   moreover
   obtain agg_op d where omega_def:"\<omega> = (agg_op,d)" using Agg.prems(1) by cases auto
   moreover
-  have eval_agg_op_case: "eval_agg_op (agg_op,d) M = eval_agg_op' (agg_op,d) M" for M  using omega_def eval_agg_op_sound apply auto sorry
+  have eval_agg_op_case: "M = {(x, ecard Zs) | x Zs. Zs = {zs. length zs = length tys \<and> Formula.sat \<sigma> V (zs @ v) i \<phi> \<and> Formula.eval_trm (zs @ v) f = x} \<and> Zs \<noteq> {}}
+ \<Longrightarrow> eval_agg_op (agg_op,d) M = eval_agg_op' (agg_op,d) M" for M
+    apply (rule eval_agg_op_sound) using omega_def nfv Agg(3,4,6,8) by auto 
   ultimately show ?case by auto
 
 next
@@ -287,143 +295,5 @@ next
   then show ?case  using match_cong[OF refl other_IH, where ?r=r] by auto 
 qed (auto elim: wty_formula.cases split: nat.splits)
 
-(*
-
- using assms  proof (induction S E \<phi>  arbitrary: v V i rule: wty_formula.induct)
-
-  case (Pred S p tys E tms )
-  from  Pred  have tms_wty: "x \<in> set tms \<Longrightarrow> \<exists>t \<in> set tys. E \<turnstile> x :: t " for x by (metis Pred.hyps(2) in_set_conv_nth list_all2_conv_all_nth)
-   have eval_tms_eq: "map (Formula.eval_trm v) tms = map (eval_trm' v) tms" using  tms_wty Pred(3) apply auto
-     subgoal for x using eval_trm_sound[of E x _ v] by auto done
-  then show ?case apply auto by (metis eval_tms_eq )+
-next
-  case (Eq E x t y S)
-  then show ?case using eval_trm_sound by auto 
-next
-  case (Less E x1 t x2)
-  then show ?case using eval_trm_sound  ty_of_eval_trm value_of_eval_trm[of E x2 v  ] value_of_eval_trm[of E x1 v  ] 
-    by (cases t) (auto simp add: undef_less_def undef_less_eq_sound less_event_data_def)
-next
-  case (LessEq E x1 t x2)
-  then show ?case using eval_trm_sound  ty_of_eval_trm value_of_eval_trm[of E x2 v  ] value_of_eval_trm[of E x1 v  ]
-    by (cases t) (auto simp add: undef_less_eq_sound) 
-next 
-  case (Let S E \<phi> p E' \<psi>)
-  {
-    fix v' i'
-    assume assm: " Formula.sat \<sigma> V v' i' \<phi>"
-    have "\<forall>y\<in>fv \<phi>. ty_of (v' ! y) = E y" sorry
-    then have "local.sat' \<sigma> V v' i' \<phi>" using Let.IH[of v' V i'] assm by auto
-  }
- moreover{
-    fix v' i'
-    assume assm: "local.sat' \<sigma> V v' i' \<phi>"
-    have "\<forall>y\<in>fv \<phi>. ty_of (v' ! y) = E y" sorry
-    then have "Formula.sat \<sigma> V v' i' \<phi>" using Let.IH[of v' V i'] assm by auto
-  }
-  ultimately have "length v' = Formula.nfv \<phi> \<Longrightarrow>  Formula.sat \<sigma> V v' i' \<phi> =  local.sat' \<sigma> V v' i' \<phi>" for v' i' by auto
-  from this  have " (\<lambda>a. if a = p then Some (\<lambda>i. {v. length v = Formula.nfv \<phi> \<and> Formula.sat \<sigma> V v i \<phi>}) else V a) 
-  = V(p \<mapsto> \<lambda>i. {v. length v = Formula.nfv \<phi> \<and> local.sat' \<sigma> V v i \<phi>})" by fastforce
-
-  then show ?case using Let by auto
-next
-  case (Agg E y agg_type t tys f S \<phi> d)
-  then show ?case  apply auto sorry
-
-next
-  case (Neg S E \<phi>)
-  from Neg.prems(2) have "safe_formula \<phi> \<or> ?case" using safe_neg_eq by (cases \<phi>)  auto 
-  then show ?case using Neg by auto
-
-next
-  case (And S E \<phi> \<psi>)
-  have phi_eq: "Formula.sat \<sigma> V v i \<phi> \<longleftrightarrow> sat' \<sigma> V v i \<phi>" using And by auto
-  have "\<psi> = Formula.Eq(Formula.Var x1 ) t \<or> \<psi> = Formula.Eq t (Formula.Var x1 )  \<Longrightarrow> safe_assignment (fv \<phi>) \<psi> \<Longrightarrow> Formula.eval_trm v t = eval_trm' v t " for x1 t
-    using And(2,5) eval_trm_sound by (auto simp add: safe_assignment_def elim!: wty_formula.cases)
-  then  have safe_case: " safe_assignment (fv \<phi>) \<psi> \<Longrightarrow> safe_formula \<psi> \<or>?case " apply (cases \<psi>) using And(2,5) phi_eq
-    by (auto simp add: safe_assignment_def split: trm.splits elim!: wty_formula.cases)
-  have is_const_case: "is_constraint \<psi> \<Longrightarrow> ?case" using phi_eq And(2,5-6)  eval_trm_sound[where ?E=E] undef_less_eq_sound apply (cases \<psi> rule: is_constraint.cases)
-                    apply (auto simp add: undef_less_def elim: wty_formula.cases) 
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   by (auto  elim!: wty_formula.cases  )
-    subgoal for x41 x42   by (auto  elim!: wty_formula.cases  )
-    subgoal for x41 x42   by (auto  elim!: wty_formula.cases  )
-    subgoal for x41 x42   by (auto  elim!: wty_formula.cases  )
-    subgoal for x41 x42   by (auto  elim!: wty_formula.cases  )
-    subgoal for x41 x42   by (auto  elim!: wty_formula.cases  )
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    subgoal for x41 x42   apply (auto  elim!: wty_formula.cases  ) apply (drule poly_value_of[where ?v=v]) apply (auto simp add: less_event_data_def) done
-    done
-
-  have "safe_formula \<psi> \<or>  (case \<psi> of formula.Neg x \<Rightarrow> safe_formula x | _ \<Rightarrow> False) \<or> ?case" 
-    using And.prems(2) safe_case is_const_case  by auto
-  moreover have "safe_formula \<psi> \<Longrightarrow> ?case" using And  by auto 
-  ultimately show ?case apply (auto ) sorry
-next
-  case (Ands \<phi>s S E)
-  then show ?case sorry
-next
-  case (Exists S t E \<phi> )
-   {
-    fix za
-    assume  assm: "Formula.sat \<sigma> V (za # v) i \<phi>" 
-    have nfv: " Formula.nfv \<phi> \<le> Suc (length v)" using Exists(6) nfv_exists[of \<phi> t] by auto
-    have "0 \<in> fv \<phi> \<Longrightarrow> ty_of za = t" 
-      using ty_of_sat_safe[where ?E="case_nat t E" and ?S=S and ?\<phi>=\<phi> and ?v="za#v" and ?V=V and ?i=i and ?\<sigma>=\<sigma> and ?x=0]  
-      Exists(1,4-5)  nfv assm by auto 
-    then have "\<forall>y\<in>fv \<phi>. ty_of ((za # v) ! y) = (case y of 0 \<Rightarrow> t | Suc x \<Rightarrow> E x)" using Exists(3)   by (auto simp add:  fvi_Suc split: nat.splits )
-
-    from this  have "local.sat' \<sigma> V (za # v) i \<phi>" using Exists.IH[of "za#v" V i] Exists(4-5) nfv assm by auto
-  }
-  moreover {
-    fix za
-   assume  assm: "local.sat' \<sigma> V (za # v) i \<phi>" 
- have nfv: " Formula.nfv \<phi> \<le> Suc (length v)" using Exists(6) nfv_exists[of \<phi> t] by auto
-    have "0 \<in> fv \<phi> \<Longrightarrow> ty_of za = t" 
-      using ty_of_sat'_safe[where ?E="case_nat t E" and ?S=S and ?\<phi>=\<phi> and ?v="za#v" and ?V=V and ?i=i and ?\<sigma>=\<sigma> and ?x=0]  
-      Exists(1,4-5)  nfv assm by auto
-    from this have "\<forall>y\<in>fv \<phi>. ty_of ((za # v) ! y) = (case y of 0 \<Rightarrow> t | Suc x \<Rightarrow> E x)" using Exists(3) assm by (auto simp add: fvi_Suc split: nat.splits )
-    from this have " Formula.sat \<sigma> V (za # v) i \<phi>" using Exists.IH[of "za#v" V i] Exists(4-6) nfv_exists[of \<phi> t] assm by auto
-  }
-  ultimately show ?case   by auto 
-
-next
-
-  case (Since S E \<phi> \<psi> \<I>)
-  then show ?case sorry
-next
-  case (Until S E \<phi> \<psi> \<I>)
-  then show ?case sorry
-next
-
-  case (MatchF S E x2 x1) 
-  from this  have other_IH: "\<phi> \<in> regex.atms x2 \<Longrightarrow> Formula.sat \<sigma> V5 v i5 \<phi> = local.sat' \<sigma> V5 v i5 \<phi>" for \<phi> V5 i5 
-    using MatchF apply (auto simp add: regex.pred_set fv_regex_alt) sorry
-  then show ?case  using match_cong[OF refl other_IH, where ?r=x2] by auto 
-next
-  case (MatchP S E x2 x1)
-    from this  have other_IH: "\<phi> \<in> regex.atms x2 \<Longrightarrow> Formula.sat \<sigma> V5 v i5 \<phi> = local.sat' \<sigma> V5 v i5 \<phi>" for \<phi> V5 i5 
-    apply (auto simp add: regex.pred_set fv_regex_alt) sorry
-  then show ?case  using match_cong[OF refl other_IH, where ?r=x2] by auto 
-qed  (auto split: nat.splits) 
-
-*)
 end
 end
